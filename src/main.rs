f7e065452e59697112e3bf2110ffaa61913d1b11
Thomas Daede <daede003@umn.edu>
2016-01-22 15:53:30 -0800

    Update gtk versions

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,11 @@ use std::env;
 fn main() {
     gtk::init().unwrap_or_else(|_| panic!("Failed to initialize GTK."));
 
-    let window = gtk::Window::new(gtk::WindowType::TopLevel).unwrap();
+    let window = gtk::Window::new(gtk::WindowType::Toplevel).unwrap();
 
     window.set_title("mpv-rice");
     window.set_border_width(10);
-    window.set_window_position(gtk::WindowPosition::Center);
+    // window.set_window_position(gtk::WindowPosition::Center);
     window.set_default_size(350, 70);
 
     window.connect_delete_event(|_, _| {
